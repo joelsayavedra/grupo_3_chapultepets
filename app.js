a46b37d5427db91073ace7835ab6c1e809e9561b
joelsayavedra <joelsayavedra112@gmail.com>
2021-06-26 10:52:44 -0500

    resolviendo conflictos

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 let express = require('express');
 let app = express();
 
-<<<<<<< HEAD
 app.listen(process.env.PORT || 3000, function(){
     return console.log("Servidor corriendo");
-=======
-app.listen(3000, function(){
-    return console.log("Servidor operativo y funcionando en 3000");
->>>>>>> d528555a047f86c68146e85d54375cfe344f29e3
+
 }); //El servidor inicia, para pararlo, usar Ctrl+C en VSC
 
 app.get('/', function(req, res){
