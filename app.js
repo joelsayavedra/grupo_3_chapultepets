4491eacc300f8f5aa3e422e1da431435d88f94b7
joelsayavedra <joelsayavedra112@gmail.com>
2021-06-26 10:43:30 -0500

    inicio modificaciones heroku

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let express = require('express');
 let app = express();
-let saludo = 'Holis UuU';
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     return console.log("Servidor corriendo");
 }); //El servidor inicia, para pararlo, usar Ctrl+C en VSC
 
