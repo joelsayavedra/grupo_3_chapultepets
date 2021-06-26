d528555a047f86c68146e85d54375cfe344f29e3
Eduardodac <eduardo.dac@outlook.com>
2021-06-26 10:41:12 -0500

    Carpetas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let app = express();
 let saludo = 'Holis UuU';
 
 app.listen(3000, function(){
-    return console.log("Servidor corriendo");
+    return console.log("Servidor operativo y funcionando en 3000");
 }); //El servidor inicia, para pararlo, usar Ctrl+C en VSC
 
 app.get('/', function(req, res){
