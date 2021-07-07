cf64dcce201334336238dc18da35780b6787f95b
joelsayavedra <joelsayavedra112@gmail.com>
2021-07-06 20:23:45 -0500

    indexController creado

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path=require('path');
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", "./src/views")
 
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
