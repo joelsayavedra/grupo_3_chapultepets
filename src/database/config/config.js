28c6b01d205a6bf39c07ef6e973d39002fc56b25
joelsayavedra <joelsayavedra112@gmail.com>
2021-08-19 20:32:05 -0500

    sequelize instalado y vinculado a base de datos en JawsDB

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports={
+  "development": {
+    "username": "ifzc3sn9asfvlmap",
+    "password": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "database": "veewykwetvy92l60",
+    "host": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "ifzc3sn9asfvlmap",
+    "password": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "database": "veewykwetvy92l60",
+    "host": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "ifzc3sn9asfvlmap",
+    "password": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "database": "veewykwetvy92l60",
+    "host": "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql"
+  }
+}
