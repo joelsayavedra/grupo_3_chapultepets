3cfc66c13944c3a3cca425e7119e0fc2f3abc40f
joelsayavedra <joelsayavedra112@gmail.com>
2021-10-21 10:44:47 -0500

    El carrito ya es dinámico, se pueden agregar productos desde la vista de detalle y se verán en el carrito. Se pueden eliminar artículos y cambiar la cantidad

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -2,6 +2,13 @@ window.addEventListener("load",e=>{
     // fetch
     let id = window.location.pathname;
     id=id.substring(10,id.length);
+    let carrito = [];
+    // sessionStorage.clear();
+
+    if (sessionStorage.getItem("carrito",carrito)) {
+        carrito = JSON.parse(sessionStorage.getItem("carrito",carrito));
+    }
+    console.log(carrito);
 
     let elBoton = document.querySelector("button#AgregarAlCarrito");
     let elInput = document.querySelector("input#AgregarAlCarrito");
@@ -13,8 +20,12 @@ window.addEventListener("load",e=>{
         if(Number.isInteger(parseFloat(elInput.value))){
             warning.innerHTML ="";
             console.log("es entero");
-            sessionStorage.setItem("id",id);
-            sessionStorage.setItem("amount",elInput.value);
+            let newItem = {
+                id: id,
+                amount: elInput.value
+            }
+            carrito.push(newItem);
+            sessionStorage.setItem("carrito",JSON.stringify(carrito));
             window.location.href = "http://localhost:3000/products/cart";
 
         }else{
